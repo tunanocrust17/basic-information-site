fe5199ae822db236bcfafada818747eef90f8973
Eric Huet <ewhuet@gmail.com>
2024-08-06 11:22:51 +0300

    refactored to use express

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname,'../frontend', 'index.html')))
+app.get('/about', (req, res) => res.sendFile(path.join(__dirname, '../frontend', 'about.html')))
+app.get('/contact-me', (req, res) => res.sendFile(path.join(__dirname, '../frontend', 'contact-me.html')))
+
+const PORT = 3000
+app.listen(PORT, () => console.log(`Basic informational site listening on ${PORT}`))
