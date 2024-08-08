92c4f73df7d87d984df259becdf2323a60730eae
Eric Huet <ewhuet@gmail.com>
2024-08-08 11:50:20 +0300

    created a route to catch any routes that don't match a current path

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const app = express()
 app.get('/', (req, res) => res.sendFile(path.join(__dirname,'../frontend', 'index.html')))
 app.get('/about', (req, res) => res.sendFile(path.join(__dirname, '../frontend', 'about.html')))
 app.get('/contact-me', (req, res) => res.sendFile(path.join(__dirname, '../frontend', 'contact-me.html')))
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../frontend', '404.html')))
 
 const PORT = 3000
 app.listen(PORT, () => console.log(`Basic informational site listening on ${PORT}`))
